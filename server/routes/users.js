c92477fa743bc7543a6305e8edbf0e04ee5ac307
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-11-20 14:51:09 +0000

    user login endpoint created.

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,3 +20,32 @@ router.post("/register", async (req, res) => {
     res.send(error);
   }
 });
+
+router.post("/login", async (req, res, next) => {
+  //checks if all the input feilds are filled
+  if (Object.values(req.body).includes("")) {
+    return res.status(400).send("Inputs cannot be empty.");
+  }
+  try {
+    //Find the user
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!user) {
+      // checks if the user does not exist
+      return res.status(404).send("Invalid user");
+    } else {
+      // checks if the passwords match
+      if (user.password === req.body.password) {
+        return res.send(user);
+      } else {
+        return res.status(400).send("Incorrect password");
+      }
+    }
+  } catch (error) {
+    //send any erros
+    res.send(error);
+  }
+});

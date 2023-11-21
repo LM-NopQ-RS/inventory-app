f40c2ef2a34355e09a282beb9e9d621be51fb473
LoisClinton <loisclinton@hotmail.co.uk>
2023-11-21 21:17:19 +0000

    styling for DetailsPage

--- public/react/pages/DetailsPage.jsx
+++ public/react/pages/DetailsPage.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import apiURL from "../api";
 
 function DetailsPage() {
+  const { id } = useParams();
+  const [item, setItem] = useState({});
+
+  useEffect(() => {
+    const getItem = async () => {
+      try {
+        const response = await fetch(`${apiURL}/items/${id}`);
+        const itemData = await response.json();
+        setItem(itemData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getItem();
+  }, [id]);
+
   return (
-    <>
+    <div className="details-container">
       <button>BACK</button>
       <section className="created-details">
         <div>
           <h3>Created on:</h3>
-          <p>this date</p>
+          <p>{item.createdOn}</p>
         </div>
         <div>
           <h3>By User:</h3>
-          <p>this user</p>
+          <p>{item.createdBy}</p>
         </div>
       </section>
-      <section className="item-details-content">
-        <h2>Name: {"item name"}</h2>
-        <p>Price: {"item price"}</p>
-        <p>Description: {"item description"}</p>
-        <p>Category: {"item category"}</p>
-        <img>{"item image"}</img>
-      </section>
-    </>
+      <div className="item-details-content">
+        <div className="item-details">
+          <div className="item-details-text">
+            <h2>Name: {item.name}</h2>
+            <p>Price: {item.price}</p>
+            <p>Description: {item.description}</p>
+            <p>Category: {item.category}</p>
+          </div>
+          <img src={item.image} alt={item.name} />
+        </div>
+        <div className="button-container">
+          <button>EDIT</button>
+          <button>DELETE</button>
+        </div>
+      </div>
+    </div>
   );
 }
 

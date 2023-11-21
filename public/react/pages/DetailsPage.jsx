1a8345cc61e1f5c0aef9cb0a8283d3cc4131c693
LoisClinton <loisclinton@hotmail.co.uk>
2023-11-21 16:31:40 +0000

    basic template for Details page created

--- public/react/pages/DetailsPage.jsx
+++ public/react/pages/DetailsPage.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
-function DetailsPage() {}
+function DetailsPage() {
+  return (
+    <>
+      <button>BACK</button>
+      <section className="created-details">
+        <div>
+          <h3>Created on:</h3>
+          <p>this date</p>
+        </div>
+        <div>
+          <h3>By User:</h3>
+          <p>this user</p>
+        </div>
+      </section>
+      <section className="item-details-content">
+        <h2>Name: {"item name"}</h2>
+        <p>Price: {"item price"}</p>
+        <p>Description: {"item description"}</p>
+        <p>Category: {"item category"}</p>
+        <img>{"item image"}</img>
+      </section>
+    </>
+  );
+}
 
 export default DetailsPage;

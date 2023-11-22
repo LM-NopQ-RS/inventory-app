413eced3a742b530022c491e553977f11f4235ff
LoisClinton <loisclinton@hotmail.co.uk>
2023-11-22 13:15:53 +0000

    Added functionality to the 'Edit' button on DetailsPage and added change for Mil

--- public/react/pages/DetailsPage.jsx
+++ public/react/pages/DetailsPage.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import apiURL from "../api";
+import UpdateForm from "../components/UpdateForm";
 
 function DetailsPage() {
+  const [isOpen, setIsOpen] = useState(false);
   const { id } = useParams();
   const [item, setItem] = useState({});
   const navigate = useNavigate();
@@ -23,51 +25,55 @@ function DetailsPage() {
   }, [id]);
 
   return (
-    <div className="details-container">
-      <div className="top-button-container">
-        <button
-          onClick={() => {
-            navigate("/home");
-          }}
-        >
-          Back
-        </button>
-      </div>
-      <section className="created-details">
-        <div className="created-item-container">
-          <h3>Created on:</h3>
-          <p>{item.createdAt}</p>
+    <>
+      {isOpen && <UpdateForm updatingItem={item} setIsOpen={setIsOpen} />}
+      <div className="details-container">
+        <div className="top-button-container">
+          <button
+            onClick={() => {
+              navigate("/home");
+            }}
+          >
+            Back
+          </button>
         </div>
-        <div className="created-item-container">
-          <h3>By User:</h3>
-          <p>Lois Clinton placeholder</p>
-        </div>
-      </section>
-      <div className="item-details-content">
-        <div className="item-details">
-          <div className="item-details-text">
-            <h2>{item.name}</h2>
-            <p>
-              <b className="background-grey font-russo">Price:</b> {item.price}
-            </p>
-            <p>
-              <b className="background-grey font-russo">Category:</b>{" "}
-              {item.category}
-            </p>
-            <p>
-              {" "}
-              <b className="background-grey font-russo">Description:</b>{" "}
-              {item.description}
-            </p>
+        <section className="created-details">
+          <div className="created-item-container">
+            <h3>Created on:</h3>
+            <p>{item?.createdAt}</p>
           </div>
-          <img src={item.image} alt={item.name} />
-        </div>
-        <div className="button-container">
-          <button>Edit</button>
-          <button>Delete</button>
+          <div className="created-item-container">
+            <h3>By User:</h3>
+            <p>Lois Clinton placeholder</p>
+          </div>
+        </section>
+        <div className="item-details-content">
+          <div className="item-details">
+            <div className="item-details-text">
+              <h2>{item?.name}</h2>
+              <p>
+                <b className="background-grey font-russo">Price:</b>{" "}
+                {item?.price}
+              </p>
+              <p>
+                <b className="background-grey font-russo">Category:</b>{" "}
+                {item?.category}
+              </p>
+              <p>
+                {" "}
+                <b className="background-grey font-russo">Description:</b>{" "}
+                {item?.description}
+              </p>
+            </div>
+            <img src={item?.image} alt={item?.name} />
+          </div>
+          <div className="button-container">
+            <button onClick={() => setIsOpen(true)}>Edit</button>
+            <button>Delete</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
